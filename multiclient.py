e8635abc74be0554c31c0b77169e9129c1479fcb
sakshamarora1 <sakshamarora1001@gmail.com>
2021-05-13 22:25:23 +0530

    Async client calls

--- multiclient.py
+++ multiclient.py
@@ -9,23 +9,28 @@ headers = {"content-type": content_type}
 data = open("excellent2.jpg", "rb").read()
 
 
-async def get(session: aiohttp.ClientSession) -> dict:
-    resp = await session.request("POST", url=test_url, data=data, headers=headers)
-    dataresp = await resp.status
-    print(f"Received data for {test_url}")
-    return resp
+async def make_numbers(numbers, _numbers):
+    for i in range(numbers, _numbers):
+        print(i)
+        yield i
 
 
-async def main():
+async def make_account():
     async with aiohttp.ClientSession() as session:
-        tasks = []
-        for _ in range(10):
-            tasks.append(get(session=session))
-        htmls = await asyncio.gather(*tasks, return_exceptions=True)
-        print(tasks)
+        post_tasks = []
+        # prepare the coroutines that post
+        async for x in make_numbers(1, 6):
+            post_tasks.append(do_post(session, x))
+        # now execute them all at once
+        await asyncio.gather(*post_tasks)
 
-        return htmls
 
+async def do_post(session, i):
+    async with session.post(test_url, data=data, headers=headers) as response:
+        datares = await response.content.read()
+        # print("-> Created account number %d" % x)
+        with open(f"new{i}.ttf", "wb+") as f:
+            f.write(datares)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(make_account())
